f5c5ac12ca0d354d24838088faf8f1a9c9b7953d
aronwebsolution <dev@aronwebsolutions.com>
2022-02-09 12:57:35 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Shopify app with Node and React (testing app) 🎉</Heading>
+  </Page>
+);
+
+export default Index;
